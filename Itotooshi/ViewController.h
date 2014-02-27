d58032670b2b9835d95a99aa90d94c6282ff09ab
USER <user@USER-no-MacBook-Air-2.local>
2014-02-27 17:17:12 +0900

    Initial Commit

---
+++ Itotooshi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Itotooshi
+//
+//  Created by USER on 2014/02/27.
+//  Copyright (c) 2014年 USER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
